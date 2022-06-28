785581803e4a0b1442f6570213e331f3767ba760
n-mbachia <ndmbachia@outlook.com>
2022-06-28 21:31:42 +0000

    Task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    """
+    Fizz Buzz
+    """
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
