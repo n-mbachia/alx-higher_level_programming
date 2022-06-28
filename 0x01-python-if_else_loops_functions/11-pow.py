04f4238aa76471b993dd298498ae8f18133ce74e
n-mbachia <ndmbachia@outlook.com>
2022-06-28 21:30:53 +0000

    Task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Computes a raised to the power of b
+    Arguments:
+        a: integer
+        b: integer
+    Return:
+        The value of a ^ b
+    """
+    return a ** b
