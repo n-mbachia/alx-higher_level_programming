3ad2efd04c6ccf7bdd00035e673961fbe9b548d4
n-mbachia <ndmbachia@outlook.com>
2022-06-28 21:30:21 +0000

    Task 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Adds two integers
+    Arguments:
+        a: integer
+        b: integer
+    Return:
+        Sum of a and b
+    """
+    return a + b
