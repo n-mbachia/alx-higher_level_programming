706ebf9dac041c5d0d324f50322a4554aa27686c
n-mbachia <ndmbachia@outlook.com>
2022-07-01 21:18:47 +0000

    debugging

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name == "__main__"
+    import sys
+    no_of_args = len(sys.argv) - 1
+    if no_of_args == 1:
+        print("{} argument:".format(no_of_args))
+    else:
+        print("{} arguments:"format(no_of_args)
+
+    for i in range(1,  no_or_args + 1):
+        print("{:d}: {}".format(i, sys.arg)
