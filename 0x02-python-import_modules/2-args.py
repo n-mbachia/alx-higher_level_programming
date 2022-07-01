381abd32d5946e7d3cc01c6795d33d098e478dbe
n-mbachia <ndmbachia@outlook.com>
2022-07-01 18:54:09 +0000

    debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
     no_of_args = len(sys.argv) - 1
     if no_of_args == 1:
         print("{} argument:".format(no_of_args))
