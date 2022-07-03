306e14d9782a38bc15fa573975b9ce3c83f11b23
n-mbachia <ndmbachia@outlook.com>
2022-07-03 21:28:08 +0300

    debugging

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    else:
-        my_list.sort()
-        return((my_list[-1]))
+    max_number = float('-inf')
+    for i in my_list:
+        if (i > max_number):
+            max_number = 1
+    return max_number
