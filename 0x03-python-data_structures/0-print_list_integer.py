72d0d35d45b908563d3dc4386ff3f0decf35cd80
n-mbachia <ndmbachia@outlook.com>
2022-07-03 18:48:26 +0300

    degugging

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
-def print_list_integer(my_list=[]): #prints all integers in a list
+#!/usr/bin/python3
+def print_list_integer(my_list=[]): # prints all integers in a list
     for i in my_list:
         print('{:d}'.format(i))
