af255970ac499fa1da72c688c42c906ba86b6c55
n-mbachia <ndmbachia@outlook.com>
2022-07-03 16:09:56 +0300

    debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,5 @@ if __name__ == "__main__":
         print("{} argument:".format(no_of_args))
     else:
         print("{} arguments:".format(no_of_args))
-
-    for i in range(1,  no_of_args + 1):
-        print("{:d}: {}".format(i, sys.arg))
+    for i in range(1, no_of_args + 1):
+        print("{:d}: {}".format(i, sys.args))
