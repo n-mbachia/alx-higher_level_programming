0164472dc1d4b5cf03e1a66d25cbef60e556d674
n-mbachia <ndmbachia@outlook.com>
2022-07-04 20:21:34 +0300

    debugging

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("None")
-    for row in matrix:
+    for i in matrix:
         for col in row:
             if col == row[-1]:
                 print("{:d}".format(col), end="")
