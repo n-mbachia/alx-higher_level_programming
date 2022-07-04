c13f9aa22e7076137035161f13be9e3626c8549d
n-mbachia <ndmbachia@outlook.com>
2022-07-04 20:22:36 +0300

    debugging

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
+    for row in matrix:
         for col in row:
             if col == row[-1]:
                 print("{:d}".format(col), end="")
