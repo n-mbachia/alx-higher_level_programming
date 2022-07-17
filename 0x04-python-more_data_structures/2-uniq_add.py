2e9cbcec040e47d7e3d1b3e42888c81cb9defd31
n-mbachia <ndmbachia@outlook.com>
2022-07-17 15:17:26 +0300

    debugging

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,5 @@
-#!/usr/python3
+#!/usr/bin/python3
+
 
 def uniq_add(my_list=[]):
-    unique_list = []
-    for x in my_list:
-        if x not in unique_list:
-            unique_list.append(x)
-    return sum(unique_list)
+    return sum(set(my_list))
