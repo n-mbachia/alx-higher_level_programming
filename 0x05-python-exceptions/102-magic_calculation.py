3b01763ca308974f826c53dd042f28c4f0600e7c
n-mbachia <ndmbachia@outlook.com>
2022-07-18 16:20:28 +0300

    debugging

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
+def safe_print_integer(value):
+    """
+    Prints an integer with "{:d}".format()
 
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception("Too far")
-            else:
-                result = result + (a**b) / i
-        except:
-            result = b + a
-            break
-    return result
+    Args:
+        value: value to be printed
+
+    Return: True if successful.Otherwise False
+    """
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
+        return False
+    else:
+        return True
